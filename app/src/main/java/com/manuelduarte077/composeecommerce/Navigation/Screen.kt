ed649905167e450ee9ae74561150709792f028ee
manuelduarte077 <carlosd99m@gmail.com>
2025-06-01 04:19:47 -0600

    refactor: migrate project from Android to Kotlin Multiplatform

--- app/src/main/java/com/manuelduarte077/composeecommerce/Navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.manuelduarte077.composeecommerce.Navigation
-
-enum class Screen(val route:String) {
-    LoginScreen("login_screen"),
-    SignUpScreen("signup_screen"),
-    HomeScreen("home_screen"),
-    DetailsScreen("details_screen")
-}
